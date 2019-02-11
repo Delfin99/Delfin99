d0717befb1649bca68ecdc832c3d6c4c53413e69
Seungki Jeong <jeong@MacPro.local>
2019-02-11 20:33:53 +0000

    SSIS - Tutorial

---
+++ Study/SSIS/note/SSIS.sql
@@ -0,0 +1,7 @@
+SQL Server Integration Services(SSIS)
+
+- This is a powerful ETL Tool, which is used for building enterprise-level data transformation, and data integration solutions.
+
+- This includes many built-in tasks and transformations to solve the complex business problems by building high-performance data integration packages. 
+
+- It can be used for updating data warehouses, data mining, downloading or copying files, extract and transfer data from XML to SQL etc.
