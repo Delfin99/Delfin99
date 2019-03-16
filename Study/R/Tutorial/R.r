06f3fb112b39fa1137e6e669e5b658a20de2ac76
Delfin99 <wjdtmdrl12@gmail.com>
2019-03-16 15:51:22 +0000

    R - start R programming

---
+++ Study/R/Tutorial/R.r
@@ -0,0 +1,35 @@
+R Programming
+
+Definition:
+
+R is a programming language and environment commonly used in statistical computing, data analytics and scientific research.
+
+Why?:
+
+1. Open source
+
+2. R runs on all platforms
+
+3. Learing R will increase your chances of getting a job
+
+4. R is being used by the biggest tech giants
+
+5. Expressive syntax and easy to use interface
+
+Application of R Programming in Real World:
+
+1. Data Science
+
+2. Statistical computing
+
+3. Machine Learing
+
+Alternatives to R programming
+
+1. Python
+
+2. SAS(Statistical Analysis System)
+
+3. SPSS(Software Package for Statistical Analysis)
+
+
