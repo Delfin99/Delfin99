1afd3019acad20a55397af14bc33c534b31b3448
Delfin99 <wjdtmdrl12@gmail.com>
2019-03-23 18:10:59 +0000

    Python - matplotlib(pyplot)

---
+++ Study/Python/Tutorials/Libraries.py
@@ -0,0 +1,29 @@
+Python Libraries
+
+	- Pandas
+
+		- Pandas helps us with munging and preparing data; it it great for operating on and maintaining structured data.
+
+	- Scipy(Scientific Python)
+
+		- Scipy stands on top of Numpy. With this library, we can carry out functionality like Linear Algebra, Fourier Transform, Optimization, and many others.
+
+	- Numpy
+
+		- NumPy (Numerical Python) is another library that lets us deal with features like linear algebra, Fourier transforms and advanced random number capabilities. One very import feature of NumPy is the n-dimensional array.
+
+	- Matplotlib
+
+		- Matplotlib will let you plot different kinds of graphs. These include pie charts, bar graphs, histograms, and even heat plots.
+
+	- Scikit-learn
+
+		- Scikit-learn is great for machine learning. It will let you statistically model and implement machine learning. The tools for these include clustering, regression, classification, and dimensionality reduction.
+
+	- Seaborn
+
+		- Seaborn is good with statistical data visualization. Making use of it, we can create useful and attractive graphics.
+
+	- Scrapy
+
+		- Scrapy will let you crawl the web. It begins on a home page and gets deeper within a website for information.
